efa552dce09c80e327dc9d7ccc9bfdc11a2fe54f
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-09-02 17:59:16 +0600

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,4 +4,13 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        gig_drivers: 'gig_drivers.html',
+        business_fleet: 'business_fleet.html',
+      },
+    },
+  },
 })
