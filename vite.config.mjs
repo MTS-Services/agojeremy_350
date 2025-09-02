48586c1aae71882ebf43636ea676352963f3be14
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-09-02 18:07:11 +0600

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,6 +10,8 @@ export default defineConfig({
         main: 'index.html',
         gig_drivers: 'gig_drivers.html',
         business_fleet: 'business_fleet.html',
+        real_estate: 'real_estate.html',
+        personal_car: 'personal_car.html',
       },
     },
   },
